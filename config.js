863cbedba8b55ec17775f8fc8b33cd66fc179d89
aozora <marcello.palmitessa@gmail.com>
2013-06-28 10:06:08 +0200

    moved drex to ./lib

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ var utils = require('./lib/utils');
 var hbs = require('./lib/hbs');
 var sessionStore = new RedisStore();
 var fs = require('fs');
-var drex = require('./lib/drex.js');
+var drex = require('./lib/drex');
 // Require parameters class and instance it
 var parameters = require('./params');
 
