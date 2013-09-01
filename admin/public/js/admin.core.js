019a36958ca0170578d76746255d8284bb9cff4e
aozora <marcello.palmitessa@gmail.com>
2013-09-01 22:06:56 +0200

    wip

--- admin/public/js/admin.core.js
+++ admin/public/js/admin.core.js
@@ -6,25 +6,25 @@
    $(function() {
 
 
-      // Custom Select
-      $("select[name='herolist']").selectpicker({style: 'btn-primary', menuStyle: 'dropdown-inverse'});
-
-      // Tooltips
-      $("[data-toggle=tooltip]").tooltip("show");
+//      // Custom Select
+//      $("select[name='herolist']").selectpicker({style: 'btn-primary', menuStyle: 'dropdown-inverse'});
+//
+//      // Tooltips
+//      $("[data-toggle=tooltip]").tooltip("show");
 
 //    // Tags Input
 //    $(".tagsinput").tagsInput();
 
 
 
-      $(".btn-group a").on('click', function() {
-         $(this).siblings().removeClass("active").end().addClass("active");
-      });
+//      $(".btn-group a").on('click', function() {
+//         $(this).siblings().removeClass("active").end().addClass("active");
+//      });
 
 
 
-      // Switch
-      $("[data-toggle='switch']").wrap('<div class="switch" />').parent().bootstrapSwitch();
+//      // Switch
+//      $("[data-toggle='switch']").wrap('<div class="switch" />').parent().bootstrapSwitch();
 
    });
 
